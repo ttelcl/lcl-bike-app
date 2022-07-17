50fc292f86a888b30023e29520366b203cf78cf3
ttelcl <9429819+ttelcl@users.noreply.github.com>
2022-07-17 20:46:20 +0300

    Removed some unused code

--- UnitTests.DataWrangling/DataLoadingTests.cs
+++ UnitTests.DataWrangling/DataLoadingTests.cs
@@ -34,43 +34,6 @@ namespace UnitTests.DataWrangling
     }
 
     [Fact]
-    public void CanLoadStationsOld()
-    {
-      var df = DataFolder.LocateAsAncestorSibling("datafolder0");
-      Assert.NotNull(df);
-      Assert.Contains("/bin/", df.Root.Replace('\\', '/'));
-
-      var adapter = new StationAdapter();
-      var stations = new List<RawStation>();
-      using(var xsv = Xsv.ReadXsv(df.OpenReadText("stations-subset.csv"), ".csv").AsXsvReader())
-      {
-        foreach(var cursor in xsv.ReadCursor(adapter.ColumnMapping))
-        {
-          var station = adapter.Read(cursor);
-          if(station != null)
-          {
-            stations.Add(station);
-          }
-        }
-      }
-      Assert.NotEmpty(stations);
-      _output.WriteLine($"Read {stations.Count} stations");
-
-      var onm = "stations.json";
-      using(var ow = df.CreateWriteTextTmp(onm))
-      {
-        var ser = new JsonSerializer();
-        using(var jw = new JsonTextWriter(ow))
-        {
-          jw.Formatting = Formatting.Indented;
-          ser.Serialize(jw, stations);
-        }
-      }
-      df.BackupShuffle(onm);
-      Assert.True(df.HasFile(onm));
-    }
-
-    [Fact]
     public void CanLoadStations()
     {
       var df = DataFolder.LocateAsAncestorSibling("datafolder0");
