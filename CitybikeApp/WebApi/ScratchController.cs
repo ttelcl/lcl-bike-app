96c2e709f2157bf53fee70a62dcc3d1d62393bb4
ttelcl <9429819+ttelcl@users.noreply.github.com>
2022-07-21 21:45:42 +0300

    Added a dedicated folder / namespace for the web API stuff in the web app

--- CitybikeApp/WebApi/ScratchController.cs
+++ CitybikeApp/WebApi/ScratchController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 
-namespace CitybikeApp
+namespace CitybikeApp.WebApi
 {
   [Route("api/[controller]")]
   [ApiController]
