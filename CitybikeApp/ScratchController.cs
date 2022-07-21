eecc83b52bb59505d7256c4e19f12678008bd727
ttelcl <9429819+ttelcl@users.noreply.github.com>
2022-07-21 17:01:23 +0300

    Initial content for the "CitybikeApp" web app & web api app. ALERT!
    - ALERT! it seems the JS / CSS libraries are not included

---
+++ CitybikeApp/ScratchController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CitybikeApp
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ScratchController: ControllerBase
+  {
+
+    [Route("dummy")]
+    public Dictionary<string, string> GetDummy()
+    {
+      var d = new Dictionary<string, string>();
+      d["ServerTime"] = DateTimeOffset.Now.ToString();
+      d["RandomGuid"] = Guid.NewGuid().ToString();
+      return d;
+    }
+  }
+}
