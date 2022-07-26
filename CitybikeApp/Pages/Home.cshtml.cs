1a0c3da1ba6bab9d1e70b4add3657fd5837be9cc
ttelcl <9429819+ttelcl@users.noreply.github.com>
2022-07-26 15:38:34 +0300

    CitybikeApp: Added a copy of the ASP.NET based home page to be able to free up the root page later on

---
+++ CitybikeApp/Pages/Home.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+#pragma warning disable CS1591
+
+namespace CitybikeApp.Pages
+{
+  public class HomeModel: PageModel
+  {
+    private readonly ILogger<HomeModel> _logger;
+
+    public HomeModel(ILogger<HomeModel> logger)
+    {
+      _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+  }
+}
