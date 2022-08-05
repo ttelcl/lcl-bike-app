fe027bd377f5cd744f130c0e197e835dd8bd7b00
ttelcl <9429819+ttelcl@users.noreply.github.com>
2022-08-06 00:20:30 +0300

    Backend: new API "stationpaircounts"
    - It returns a list of triplets (departure station, return station, ride count) . Optionally
      constrained to a time range.
    - The idea is that that may be a better source for clients to calculate ride counts starting at
      or ending at a specific station.

---
+++ LclBikeApp.Database/Models/StationPairCount.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * (c) 2022  ttelcl / ttelcl
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LclBikeApp.Database.Models
+{
+  /// <summary>
+  /// A simple record for transfering a (departure station id, 
+  /// return station id, count) triplet, for reporting total ride
+  /// counts.
+  /// </summary>
+  public struct StationPairCount
+  {
+    /// <summary>
+    /// Create a new StationPairCount
+    /// </summary>
+    public StationPairCount(
+      int depId,
+      int retId,
+      int count)
+    {
+      DepId = depId;
+      RetId = retId;
+      Count = count;
+    }
+
+    /// <summary>
+    /// The departure station ID
+    /// </summary>
+    public int DepId { get; }
+
+    /// <summary>
+    /// The return station ID
+    /// </summary>
+    public int RetId { get; }
+
+    /// <summary>
+    /// The count of rides from DepId to Retid
+    /// (for some externally defined time interval)
+    /// </summary>
+    public int Count { get; }
+
+  }
+}
